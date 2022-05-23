e62044564d24d2a92f72fdfdcebcf944c41c3693
Nathan Reyes <nathanreyes.me@gmail.com>
2022-05-23 17:36:30 -0500

    Docs updates

--- docs/app.vue
+++ docs/app.vue
@@ -1,5 +1,9 @@
 <template>
   <div>
-    <MediaQuery />
+    <header class="flex">
+      <NuxtLink to="/media-query">Media Query</NuxtLink>
+      <NuxtLink to="/screens">Screens</NuxtLink>
+    </header>
+    <NuxtPage />
   </div>
 </template>
---
+++ docs/pages/screens.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h3>Screens</h3>
+  </div>
+</template>
