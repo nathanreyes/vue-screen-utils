4f107746814cd98798ba7659ee903864eef2ec6f
Nathan Reyes <nathanreyes.me@gmail.com>
2022-05-23 21:40:31 -0500

    main.ts => index.ts

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import typescript from 'rollup-plugin-typescript2';
 import clear from 'rollup-plugin-clear';
 
 export default {
-  input: 'src/main.ts',
+  input: 'src/index.ts',
   output: [
     {
       format: 'esm',
