2ce085c9487adffc50f8ae34f0d09cb5243d7d90
Nathan Reyes <nathanreyes.me@gmail.com>
2022-05-28 14:43:16 -0500

    Build types

--- rollup.config.js
+++ rollup.config.js
@@ -1,25 +1,36 @@
 import clear from 'rollup-plugin-clear';
 import typescript from '@rollup/plugin-typescript';
+import dts from 'rollup-plugin-dts';
 
-export default {
-  input: 'out-tsc/index.js',
-  output: [
-    {
-      format: 'esm',
-      file: 'dist/lib.mjs',
-      sourcemap: true,
+export default [
+  {
+    input: 'out-tsc/index.js',
+    output: [
+      {
+        format: 'esm',
+        file: 'dist/index.mjs',
+        sourcemap: true,
+      },
+      {
+        format: 'cjs',
+        file: 'dist/index.js',
+        sourcemap: true,
+      },
+    ],
+    external: ['vue'],
+    plugins: [
+      clear({
+        targets: ['./dist'],
+      }),
+      typescript(),
+    ],
+  },
+  {
+    input: 'out-tsc/dts/index.d.ts',
+    output: {
+      format: 'es',
+      file: 'dist/index.d.ts',
     },
-    {
-      format: 'cjs',
-      file: 'dist/lib.js',
-      sourcemap: true,
-    },
-  ],
-  external: ['vue'],
-  plugins: [
-    clear({
-      targets: ['./dist'],
-    }),
-    typescript(),
-  ],
-};
+    plugins: [dts()],
+  },
+];
