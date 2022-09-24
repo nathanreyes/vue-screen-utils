a5455c961cee1ea8750ddb75f26bd77e553215be
Nathan Reyes <nathanreyes.me@gmail.com>
2022-09-24 13:28:11 -0500

    demo updates

--- demo/src/components/ResizeObserver.vue
+++ demo/src/components/ResizeObserver.vue
@@ -1,26 +1,16 @@
-<script setup>
+<script setup lang="ts">
 import { ref } from 'vue';
 import { useResizeObserver } from '@vsu';
-import Resizer from './Resizer.vue';
 
 const textEl = ref(null);
-const { width, height, top, right, bottom, left, x, y, rect, cleanup } = useResizeObserver(textEl);
+const { rect, cleanup } = useResizeObserver(textEl);
 </script>
 <template>
   <div>
-    <div><strong>Width</strong>: {{ width }}</div>
-    <div><strong>Height</strong>: {{ height }}</div>
-    <div><strong>Top</strong>: {{ top }}</div>
-    <div><strong>Right</strong>: {{ right }}</div>
-    <div><strong>Bottom</strong>: {{ bottom }}</div>
-    <div><strong>Left</strong>: {{ left }}</div>
-    <div><strong>X</strong>: {{ x }}</div>
-    <div><strong>Y</strong>: {{ y }}</div>
-    <button @click="cleanup">Cleanup</button>
+    <textarea ref="textEl">{{ rect }}</textarea>
   </div>
   <div>
-    <!-- <textarea ref="textEl">{{ rect }}</textarea> -->
-    <Resizer ref="textEl">{{ rect }}</Resizer>
+    <button @click="cleanup">Cleanup</button>
   </div>
 </template>
 <style>
